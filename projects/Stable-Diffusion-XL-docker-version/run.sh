3515543ca28a3c57cdd17501c4e182933339ae95
Bili-Sakura <22338035@zju.edu.cn>
2024-07-10 11:15:27 +0800

    delete sd-xl-docker version project

--- projects/Stable-Diffusion-XL-docker-version/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# @author Loreto Parisi (loretoparisi at gmail dot com)
-#
-
-IMAGE_NAME=$1
-docker run --platform linux/amd64 -v $(pwd):/opt/program -v $(pwd)/checkpoints:/opt/program/checkpoints -p 8501:8501 --gpus all --rm -it --shm-size 16G $IMAGE_NAME bash
